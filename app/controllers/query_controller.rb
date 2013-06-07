1e69244dcdf76c66a9f06ab4b6e1e4f7b6ee2d9b
xinranw <xinranw@seas.upenn.edu>
2013-06-07 13:26:06 -0400

    Added comparison capabilities

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -4,15 +4,29 @@ class QueryController < ApplicationController
     puts params[:time_format]
     puts params[:start_date]
     puts params[:end_date]
-    @results2 = Query.tracking_data(
+    puts params[:comp]
+    puts params[:start_date2]
+    puts params[:end_date2]
+
+    @results = [Query.tracking_data(
       params[:source], 
       params[:time_format], 
       params[:start_date],
-      params[:end_date])
-    if request.xhr?
-      render :json => @results2
+      params[:end_date]
+    )]
+    
+    if (params[:comp] == "true")
+      @results = @results.append(Query.tracking_data(
+        params[:source], 
+        params[:time_format], 
+        params[:start_date2],
+        params[:end_date2]))
     end
-    return @results2
+
+    if request.xhr?
+      render :json => @results
+    end
+    return @results
   end
 
 	def index
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,55 +1,50 @@
 class Query < ActiveRecord::Base
 	# attr_accessible :title, :body
   def self.tracking_data(source, time_format, start_date, end_date)
+
     case source
-      when "total_rev"
-      when "bookings_rev" 
-        then table_name = "bookings"  
-      when "loot_rev"
+      when "bookings_rev"
+        y = "sum(price)/1000"  
+        type = "BookingInvoiceItem"
+      when "loots_rev"
+        y = "sum(price * commission_rate / 100) / 1000"
+        type = "LootServiceInvoiceItem"
       when "bookings"
-        then table_name = "bookings"
-      when "loot"  
+        y = "count(price)"
+        type = "BookingInvoiceItem"
+      when "loots"  
+        y = "count(price)"
+        type = "LootServiceInvoiceItem"
     end
 
     case time_format
       when "hour"
-        booked_at = "TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(booked_at) DIV 3600) * 3600 ))"
-        group_by = "date(booked_at), hour(booked_at)"
+        created_at = "TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(created_at) DIV 3600) * 3600 ))"
+        group_by = "hour(created_at)"
       when "day"
-        booked_at = "date(booked_at)"
-        group_by = "date(booked_at)"
+        created_at = "date(created_at)"
+        group_by = "date(created_at)"
       when "month"
-        booked_at = "CONCAT(year(booked_at), '-', monthname(booked_at))"
-        group_by = "year(booked_at), month(booked_at)"
+        created_at = "CONCAT(year(created_at), '-', monthname(created_at))"
+        group_by = "year(created_at), month(created_at)"
       when "year"
-        booked_at = "year(booked_at)"
-        group_by = "year(booked_at)"
+        created_at = "year(created_at)"
+        group_by = "year(created_at)"
     end
+
+    table_name = "invoice_items"
 
     @sql = "
       SELECT 
-        #{booked_at} as counttime, count(id) as countnum
+        #{created_at} as x, #{y} as y
       FROM 
         #{table_name}
       WHERE 
-        date(booked_at) >= '#{start_date}' && date(booked_at) <= '#{end_date}'
+        date(created_at) >= '#{start_date}' && date(created_at) <= '#{end_date}' && type = '#{type}'
       GROUP BY
         #{group_by}
       "
     return connection.select_all(@sql).first(100)
-  end
-
-
-  def self.get_time_series(from, to, period, type_of_data)
-  end
-
-  # Last resort or maybe all other methods end up calling this method?
-  # ==Parameters:
-  # sql::
-  #   User specified sql statement
-  # type_of_data::
-  #   One of the following :proportional_chart, :line
-  def self.get_data_advanced(sql, type_of_data) 
   end
 
 	def self.get_data1
@@ -71,31 +66,4 @@ class Query < ActiveRecord::Base
         GROUP BY TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(created_at) DIV 3600) * 3600 ) )")
     return @alldata2
   end
-
-  def self.get_data3(select_clause, from_clause, where_clause, group_by_clause)
-    sql = "
-      SELECT 
-        #{select_clause}
-      FROM 
-        bookings
-      WHERE 
-        year(created_at) >= 2013
-      GROUP BY 
-        TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(created_at) DIV 3600) * 3600 ) )
-      "
-
-    @alldata2 = connection.select_all("
-      SELECT 
-        TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(created_at) DIV 3600) * 3600 )) as ctime
-        , count(id) as countnum
-      FROM 
-        bookings
-      WHERE 
-        year(created_at) >= 2013
-      GROUP BY 
-        TIME(FROM_UNIXTIME((UNIX_TIMESTAMP(created_at) DIV 3600) * 3600 ) )
-      ")
-    return @alldata2
-  end
-
 end
